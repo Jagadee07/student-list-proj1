e0692c8000e8c4fdd27f1ec762175b57762ab60d
K Jagadeesh <jagadeeshkantamani@gmail.com>
2026-08-16 13:33:03 +0530

    Fix import case for Student model

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -1,4 +1,4 @@
-const Student = require("../models/Student");
+const Student = require("../models/student");
 const cloudinary = require("../config/cloudinary");
 
 const createStudent = async (req, res) => {
